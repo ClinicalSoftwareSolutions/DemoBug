1bd6c814be0630b6c51fcbef3d7219b06db49754
Neville Dastur <neville@dastur.me.uk>
2013-06-03 15:42:40 +0100

    Exmample showing template based crash using listview templates

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,28 @@
-function doClick(e) {  
-    alert($.label.text);
+$.view1 = null;
+$.view2 = null;
+
+$.init = function() {
+	$.view1 = Alloy.createController("listview", {}).getView();
+	$.wrapper.add($.view1);
 }
 
-$.index.open();
+$.init();
+$.win.open();
+
+Ti.App.addEventListener("app:showoverlay",function(){
+   $.overlayScreen();
+});
+
+$.overlayScreen = function() {
+	$.view2 = Ti.UI.createView( {height: "100%", width: "100%", backgroundColor: "#fff"} );
+	var closeBut = Ti.UI.createButton( {title: "Close"} );
+	$.view2.add(closeBut);
+
+	closeBut.addEventListener("click",function(){
+		$.wrapper.remove($.view2);
+		$.wrapper.add($.view1);
+	});
+
+	$.wrapper.add($.view2);
+	$.wrapper.remove($.view1);
+}
---
+++ app/controllers/listview.js
@@ -0,0 +1,18 @@
+var data = [];
+for (var i = 0; i < 10; i++) {
+   data.push({
+      name: { text: 'Row ' + (i + 1) },
+      image: { image: 'KS_nav_ui.png' },
+      properties: {
+         itemId: 'row' + (i + 1),
+         accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_NONE
+      }
+   });
+}
+
+var section = Ti.UI.createListSection({items: data});
+$.list.sections = [section];
+
+$.list.addEventListener('itemclick', function(e){
+	Ti.App.fireEvent("app:showoverlay",{})
+});
